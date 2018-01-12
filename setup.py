1ce7970fae9cb4d4c3f1ddafd6b2c30f16ea250a
MarcusMunch <marcus@marcusmunch.dk>
2018-01-12 18:00:40 +0100

    Changed single quotes to double quotes

--- setup.py
+++ setup.py
@@ -3,22 +3,20 @@
 
 from setuptools import setup
 
-setup(name='dotatools',
-	version='0.1',
-	description='OpenDota API wrapper',
-	long_description='dotatools is an API wrapper for the open API supplied by OpenDota',
-	url='http://github.com/marcusmunch/dotatools',
+setup(name="dotatools",
+	version="0.1",
+	description="OpenDota API wrapper",
+	long_description="dotatools is an API wrapper for the open API supplied by OpenDota",
+	url="http://github.com/marcusmunch/dotatools",
+	license="GPLv3",
 	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Programming Language :: Python'
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Programming Language :: Python"
 	],
-	author='Marcus Grünewald',
-	author_email='marcus@marcusmunch.dk',
-	license='GNU General Public License v3.0',
-	packages=['dotatools'],
-	install_requires=[
-		'requests',
-	],
-	zip_safe=False)
+	author="Marcus Grünewald",
+	author_email="marcus@marcusmunch.dk",
+	packages=["dotatools"],
+	install_requires=["requests"]
+	)
