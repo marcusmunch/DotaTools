b5445840c1dde13ac28cb6f4ac6d7e6b53808669
MarcusMunch <marcus@marcusmunch.dk>
2018-02-19 22:23:33 +0100

    PEP8 formatting

--- test/test2.py
+++ test/test2.py
@@ -8,15 +8,17 @@ failcount = 0
 id_lookup = 0
 
 while failcount < 10:
-	h = dotatools.Hero(id_lookup)
+    h = dotatools.Hero(id_lookup)
 
-	if h.localname:
-		if not "requests_cache" in sys.modules:
-			print("ID {:03d}: {:<25} 'Code' name: {}".format(id_lookup, h.localname, h.name.title()))
-		else:
-			pub_wr = '{:.2f}%'.format(h.stats["pub_winrate"])
-			print("ID {:03d}: {:<20}{} wins - 'Code' name: {}".format(id_lookup, h.localname, pub_wr, h.name.title()))
-	else:
-		failcount += 1
+    if h.localname:
+        if not "requests_cache" in sys.modules:
+            print("ID {:03d}: {:<25} 'Code' name: {}".format(
+                id_lookup, h.localname, h.name.title()))
+        else:
+            pub_wr = '{:.2f}%'.format(h.stats["pub_winrate"])
+            print("ID {:03d}: {:<20}{} wins - 'Code' name: {}".format(id_lookup,
+                                                                      h.localname, pub_wr, h.name.title()))
+    else:
+        failcount += 1
 
-	id_lookup += 1
+    id_lookup += 1
